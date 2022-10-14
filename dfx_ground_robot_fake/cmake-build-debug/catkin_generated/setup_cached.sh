3633f7170581cb13273c5302f30ba1d8ac67731b
JungSu-Yun <31683091+Jungsu-Yun@users.noreply.github.com>
2022-10-14 14:48:46 +0900

    hihi

--- dfx_ground_robot_fake/cmake-build-debug/catkin_generated/setup_cached.sh
+++ dfx_ground_robot_fake/cmake-build-debug/catkin_generated/setup_cached.sh
@@ -9,6 +9,7 @@
 # modified environment variables
 export CMAKE_PREFIX_PATH="/home/jungsu/catkin_ws/src/dfx_ground_robot/dfx_ground_robot_fake/cmake-build-debug/devel:$CMAKE_PREFIX_PATH"
 export LD_LIBRARY_PATH='/home/jungsu/catkin_ws/src/dfx_ground_robot/dfx_ground_robot_fake/cmake-build-debug/devel/lib:/home/jungsu/catkin_ws/devel/lib:/opt/ros/noetic/lib:/usr/local/cuda-11.3/lib64:/usr/local/cuda-11.3/targets/x86_64-linux/lib'
+export PATH='/home/jungsu/catkin_ws/devel/bin:/opt/ros/noetic/bin:/home/jungsu/.nvm/versions/node/v16.17.0/bin:/home/jungsu/anaconda3/condabin:/usr/local/cuda-11.3/bin:/home/jungsu/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:/home/jungsu/clion-2022.2.1/bin/ninja/linux'
 export PKG_CONFIG_PATH="/home/jungsu/catkin_ws/src/dfx_ground_robot/dfx_ground_robot_fake/cmake-build-debug/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
 export ROSLISP_PACKAGE_DIRECTORIES="/home/jungsu/catkin_ws/src/dfx_ground_robot/dfx_ground_robot_fake/cmake-build-debug/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
 export ROS_PACKAGE_PATH="/home/jungsu/catkin_ws/src/dfx_ground_robot/dfx_ground_robot_fake:$ROS_PACKAGE_PATH"
